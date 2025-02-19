a2882cf2fd0bfd870450d478ca3916f2c9a702fe
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2025-02-19 07:00:05 +0100

    END WORK

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,18 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Manager : MonoBehaviour
+public class WindmillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] windmills;
+    [SerializeField] private Slider[] sliders;
+    [SerializeField] private GameObject colorSquare;
+    private Renderer squareRenderer;
+    private int currentWindmill = 0;
+    private float[] speeds = new float[3];
+    private bool[] isLocked = new bool[3];
+    private float maxSpeed = 255f;
+    private float decayRate = 100f;
+
+    private void Start()
     {
-        
+        squareRenderer = colorSquare.GetComponent<Renderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (currentWindmill < windmills.Length && !isLocked[currentWindmill])
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                IncreaseSpeed(Time.deltaTime);
+            }
+            else
+            {
+                DecreaseSpeed(Time.deltaTime);
+            }
+        }
+
+        Rotate();
+    }
+
+    private void IncreaseSpeed(float deltaTime)
+    {
+        float newSpeed = Mathf.Clamp(speeds[currentWindmill] + (deltaTime * 100f), 0, maxSpeed);
+        speeds[currentWindmill] = newSpeed;
+        sliders[currentWindmill].value = newSpeed;
+    }
+
+    private void DecreaseSpeed(float deltaTime)
+    {
+        if (!isLocked[currentWindmill])
+        {
+            float newSpeed = Mathf.Clamp(speeds[currentWindmill] - (deltaTime * decayRate), 0, maxSpeed);
+            speeds[currentWindmill] = newSpeed;
+            sliders[currentWindmill].value = newSpeed;
+        }
+    }
+
+    private void Rotate()
+    {
+        for (int i = 0; i < windmills.Length; i++)
+        {
+            if (speeds[i] > 0)
+            {
+                windmills[i].transform.Rotate(Vector3.forward * speeds[i] * Time.deltaTime);
+            }
+        }
+    }
+
+    public void LockWindmill(int index)
+    {
+        if (!isLocked[index])
+        {
+            isLocked[index] = true;
+            currentWindmill++;
+        }
+    }
+
+    public void UpdateColorSquare()
+    {
+        float redValue = speeds[0] / maxSpeed;
+        float greenValue = speeds[1] / maxSpeed;
+        float blueValue = speeds[2] / maxSpeed;
+        Color newColor = new Color(redValue, greenValue, blueValue);
+        squareRenderer.material.color = newColor;
     }
 }
+
+
