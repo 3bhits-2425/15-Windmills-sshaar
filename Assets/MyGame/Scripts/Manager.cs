1bab7394e58e20cde989147742a061896951a506
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2025-02-21 12:52:43 +0100

    Readme

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -3,18 +3,40 @@ using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
+
     [SerializeField] private GameObject[] windmills;
-    [SerializeField] private Slider[] sliders;
     [SerializeField] private GameObject colorSquare;
+    private GameObject[] rotorHubs;
+    private Light[] lights;
+    private Slider[] sliders;
     private Renderer squareRenderer;
     private int windmill = 0;
-    private float[] speeds = new float[3];
-    private bool[] isLocked = new bool[3];
+    private float[] speeds;
+    private bool[] isLocked;
     private float maxSpeed = 255f;
 
     private void Start()
     {
         squareRenderer = colorSquare.GetComponent<Renderer>();
+        int windmillCount = windmills.Length;
+        rotorHubs = new GameObject[windmillCount];
+        lights = new Light[windmillCount];
+        sliders = new Slider[windmillCount];
+        speeds = new float[windmillCount];
+        isLocked = new bool[windmillCount];
+
+        for (int i = 0; i < windmillCount; i++)
+        {
+            lights[i] = windmills[i].GetComponentInChildren<Light>();
+            sliders[i] = windmills[i].GetComponentInChildren<Slider>();
+            rotorHubs[i] = windmills[i].transform.Find("RotorHub").gameObject;
+
+            lights[i].intensity = 0;
+            sliders[i].value = 0;
+
+            speeds[i] = 0;
+            isLocked[i] = false;
+        }
     }
 
     private void Update()
@@ -31,22 +53,33 @@ public class Manager : MonoBehaviour
             }
         }
         Rotate();
+        ColorSquare();
     }
 
-    private void Increase(float deltaTime)
+    private void Increase(float dT)
     {
-        float newSpeed = Mathf.Clamp(speeds[windmill] + (deltaTime * 100f), 0, maxSpeed);
+        float newSpeed = Mathf.Clamp(speeds[windmill] + (dT * 100f), 0, maxSpeed);
         speeds[windmill] = newSpeed;
-        sliders[windmill].value = newSpeed;
+
+        if (sliders[windmill] != null)
+            sliders[windmill].value = newSpeed;
+
+        if (lights[windmill] != null)
+            lights[windmill].intensity = Mathf.Clamp(newSpeed / 255, 0, 1.12f);
     }
 
-    private void Decrease(float deltaTime)
+    private void Decrease(float dT)
     {
         if (!isLocked[windmill])
         {
-            float newSpeed = Mathf.Clamp(speeds[windmill] - (deltaTime * 100), 0, maxSpeed);
+            float newSpeed = Mathf.Clamp(speeds[windmill] - (dT * 100), 0, maxSpeed);
             speeds[windmill] = newSpeed;
-            sliders[windmill].value = newSpeed;
+
+            if (sliders[windmill] != null)
+                sliders[windmill].value = newSpeed;
+
+            if (lights[windmill] != null)
+                lights[windmill].intensity = Mathf.Clamp(newSpeed / 255, 0, 1.12f);
         }
     }
 
@@ -56,28 +89,29 @@ public class Manager : MonoBehaviour
         {
             if (speeds[i] > 0)
             {
-                windmills[i].transform.Rotate(Vector3.forward * speeds[i] * Time.deltaTime);
+                rotorHubs[i].transform.Rotate(Vector3.forward * speeds[i] * Time.deltaTime);
             }
         }
     }
 
-    public void Lock(int index)
+    public void Lock(int i)
     {
-        if (!isLocked[index])
+        if (!isLocked[i])
         {
-            isLocked[index] = true;
+            isLocked[i] = true;
             windmill++;
         }
     }
 
     public void ColorSquare()
     {
-        float redValue = speeds[0];
-        float greenValue = speeds[1];
-        float blueValue = speeds[2];
-        Color newColor = new Color(redValue, greenValue, blueValue);
+        float red = speeds[0] / 255;
+        float green = speeds[1] / 255;
+        float blue = speeds[2] / 255;
+        Color newColor = new Color(red, green, blue);
         squareRenderer.material.color = newColor;
     }
 }
+
 
 
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # 15-Windmills-sshaar
+
+Funktionsweise
+
+    Windmühlensteuerung:
+
+        - Drückt man die Leertaste, beginnt die erste Windmühle sich zu drehen. Je länger die Leertaste gedrückt wird, desto schneller dreht sich die Windmühle.
+        - Die Geschwindigkeit wird visuell durch einen Slider auf der Windmühle angezeigt.
+        - Ein Licht, das sich ebenfalls auf der Windmühle befindet, wird heller, je schneller sich die Windmühle dreht. Die Lichtstärke ist direkt an die                Geschwindigkeit gekoppelt.
+
+    Lock-Button:
+
+        - Jede Windmühle hat einen Lock-Button. Drückt man diesen, wird die aktuelle Geschwindigkeit der Windmühle "eingefroren".
+        - Nachdem die Geschwindigkeit gesperrt ist, kann die nächste Windmühle gesteuert werden.
+        - Dieser Vorgang wiederholt sich für jede weitere Windmühle.
+
+    Farbe des Hintergrunds:
+
+        - Im Hintergrund befindet sich eine Box, die als Farbanzeigefeld dient. Die Farbe dieser Box wird basierend auf den Geschwindigkeiten der ersten drei Windmühlen dynamisch angepasst.
+        - Jede Windmühle steuert eine Farbkomponente:
+
+           Die erste Windmühle beeinflusst den Rotwert.
+           Die zweite Windmühle beeinflusst den Grünwert.
+           Die dritte Windmühle beeinflusst den Blauwert.
+
+    Anpassbarkeit:
+
+        - Es können beliebig viele Windmühlen hinzugefügt werden. Das System erkennt automatisch die Lichter, Slider und Rotoren der Windmühlen und passt die Steuerung - entsprechend an.
+        - Der Hintergrund wird farblich basierend auf den Geschwindigkeiten der ersten drei Windmühlen angepasst, aber alle Windmühlen können unabhängig voneinander  gesteuert werden.
+
+    Steuerung:
+
+        - Leertaste: Erhöht die Geschwindigkeit der aktuellen Windmühle.
+        - Lock-Button: Sperrt die Geschwindigkeit der aktuellen Windmühle und wechselt zur Steuerung der nächsten Windmühle.
+
+        
